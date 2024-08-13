873ac1c42a9105916614fa8ddf7eb54cf3e06839
xuyang.zhang2 <xuyang.zhang2@amh-group.com>
2024-08-13 14:31:27 +0800

    1

--- src/main/java/com/itheima/reggie/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/CategoryServiceImpl.java
@@ -1,4 +1,3 @@
-/*
 package com.itheima.reggie.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
@@ -17,9 +16,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper,Category> implements CategoryService{
 
+    @Autowired
+    private DishService dishService;
+
+    @Autowired
+    private SetmealService setmealService;
+
+    /**
+     * 根据id删除分类，删除之前需要进行判断
+     * @param id
+     */
     @Override
     public void remove(Long id) {
+        LambdaQueryWrapper<Dish> dishLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        //添加查询条件，根据分类id进行查询
+        dishLambdaQueryWrapper.eq(Dish::getCategoryId,id);
+        int count1 = dishService.count(dishLambdaQueryWrapper);
 
+        //查询当前分类是否关联了菜品，如果已经关联，抛出一个业务异常
+        if(count1 > 0){
+            //已经关联菜品，抛出一个业务异常
+            throw new CustomException("当前分类下关联了菜品，不能删除");
+        }
+
+        //查询当前分类是否关联了套餐，如果已经关联，抛出一个业务异常
+        LambdaQueryWrapper<Setmeal> setmealLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        //添加查询条件，根据分类id进行查询
+        setmealLambdaQueryWrapper.eq(Setmeal::getCategoryId,id);
+        int count2 = setmealService.count();
+        if(count2 > 0){
+            //已经关联套餐，抛出一个业务异常
+            throw new CustomException("当前分类下关联了套餐，不能删除");
+        }
+
+        //正常删除分类
+        super.removeById(id);
     }
 }
-*/
